61d37448ff9ad3da1a6c6c24b694914718906ed6
Favour James <favourjames061@gmail.com>
2024-01-19 14:46:11 -0800

    command line argument in python

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    a = len(argv) - 1
+    if a < 1:
+        print('{} arguments.'.format(a))
+    elif a >= 1:
+        print('{} argument:'.format(a))
+
+        for i in range(a):
+            print('{}: {:s}'.format(i + 1, argv[i + 1]))
