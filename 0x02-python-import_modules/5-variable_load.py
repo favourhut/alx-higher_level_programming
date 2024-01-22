b522e8763d7137881a40ecc1174bacabc197b7ad
Favour James <favourjames061@gmail.com>
2024-01-21 23:00:31 -0800

    Impoerts variable

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import variable_load_5
+    from variable_load_5 import a
     new_variable = variable_load_5.a
     print("{}".fomrat(new_variable))
