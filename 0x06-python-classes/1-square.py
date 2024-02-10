873279e499573f8dc1a102fbe5df8d2bb0c383b8
Favour James <favourjames061@gmail.com>
2024-02-09 17:37:39 -0800

    Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""class Square that defines a square """
+
+
+class Square:
+    """Method definition"""
+
+    def __init__(self, size):
+
+        """Creating a private instance attribute
+        Args:
+        size: The size of the square
+        """
+        self.__size = size
