8814f7c0510756b358474cacadbbd511ad487b00
Favour James <favourjames061@gmail.com>
2024-02-16 11:59:51 -0800

    0-add_integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -8,6 +8,7 @@ This module has function that returna the sum of two integers
 
 
 def add_integer(a, b=98):
+
     """
 
     This function adds two numbers and returns the result.
@@ -25,9 +26,12 @@ def add_integer(a, b=98):
     TypeError: If either `a` or `b` is not an integer or float.
 
     """
-    if type(a) not in [int, float]:
+    if  type(a) not in (int, float):
         raise TypeError('a must be an integer')
-    if type(b) not in [int, float]
+    if type(b) not in (int, flaot):
         raise TypeError('b must be an integer')
 
-    return (int(a) + int(b))
+    a = int
+    b = int
+
+    return (a + b)
