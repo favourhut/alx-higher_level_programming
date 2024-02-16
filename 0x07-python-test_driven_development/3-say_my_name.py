bcd460d52ab01fadd1c4f453fcd4c42ed52618b5
Favour James <favourjames061@gmail.com>
2024-02-16 14:28:03 -0800

    3-say_my_name.py

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """
-
-This module contains a function that returns the 
+This module contains a function that returns the
 first and last name
 
 """
+
 
 def say_my_name(first_name, last_name=""):
 
@@ -28,4 +28,4 @@ def say_my_name(first_name, last_name=""):
     if not isinstance(last_name, str):
         raise TypeError('last_name must be a string')
 
-    print('My name is {} {}'.format(first_name, last_name))
+    print('My name is {:s} {:s}'.format(first_name, last_name))
