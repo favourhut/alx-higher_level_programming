a3cbc70ba4db0f0a0e023ba7eca73dddd109de1f
Favour James <favourjames061@gmail.com>
2024-02-24 04:36:12 -0800

    6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""
+
+Creating an empty class
+
+"""
+
+
+class BaseGeometry:
+
+    """
+    creating a public instance method"""
+    def area(self):
+        raise Exception('area() is not implemented')
