b4cf95ae8da7fa2336748d3f1ac87ed548ce4049
Favour James <favourjames061@gmail.com>
2024-02-24 02:58:49 -0800

    2-is_same_class.py

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -6,7 +6,8 @@ This function checks  for same object
 
 """
 
+
 def is_same_class(obj, a_class):
 
     """This returns True of false"""
-    return(isinstance(obj, a_class))
+    return (isinstance(obj, a_class))
