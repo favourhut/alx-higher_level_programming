513a498303c08c8f8db89de7c16f31a39a688222
Favour James <favourjames061@gmail.com>
2024-02-24 05:00:45 -0800

    7-base_geometry.py

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+"""
+
+Creating an empty class
+
+"""
+
+
+class BaseGeometry:
+
+    """
+    creating a public instance method"""
+    def area(self):
+        raise Exception('area() is not implemented')
+
+    """Instance method that validates value"""
+    def integer_validator(self, name, value):
+        self.name = name
+        self.value = value
+
+        if not isinstance(value, int):
+            raise TypeError('{} must be an integer'.format(name))
+
+        if value <= 0:
+            raise ValueError('{} must be greater than 0'.format(name))
