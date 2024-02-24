647838e47ae46753dece822c0f3abb43cfbeef1c
Favour James <favourjames061@gmail.com>
2024-02-24 02:36:07 -0800

    1-my_list.py

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+
+This module prints a list but in ascending order
+
+"""
+
+
+class MyList(list):
+
+    """This functioin inhereit from list class"""
+    def print_sorted(self):
+        """this prints sorted in ascending"""
+        print(sorted(self))
