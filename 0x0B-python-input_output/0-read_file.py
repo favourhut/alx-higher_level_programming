ce8f66d262713979b91ba5eea6d9a0d5c703d179
Favour James <favourjames061@gmail.com>
2024-02-27 15:31:19 -0800

    Input and output

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -9,6 +9,12 @@ prints it out to stdout
 
 
 def read_file(filename=""):
+
+    """using with to automate the closing of the file"""
     with open(filename, encoding='UTF-8') as a_file:
+
+        """looping through the file"""
         for line in a_file:
+
+            """Printing the file out"""
             print(line, end="")
