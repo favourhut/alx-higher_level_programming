c130cb7e4bbb440b58bce1ea89b139e074c6691f
Favour James <favourjames061@gmail.com>
2024-02-27 16:32:55 -0800

    4-from_json_string.py

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""
+Write a function that returns an object
+(Python data structure) represented by a JSON string
+
+"""
+
+import json
+
+
+def from_json_string(my_str):
+
+    """Returns an object (Python data structure) rep by JSON"""
+
+    return json.loads(my_str)
