0d9eb73bf4bc4d26f120d4e985c70b2e56a7fa1b
Favour James <favourjames061@gmail.com>
2024-02-27 16:14:58 -0800

    3-to_json_string.py

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import json
+
+"""
+
+A function that returns the JASON representation of an object string
+
+"""
+
+
+def to_jason_string(my_obj):
+
+    """Returning the object representation"""
+    return jason.dumps(my_obj)
