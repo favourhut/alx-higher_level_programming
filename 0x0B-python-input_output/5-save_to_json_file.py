8357e94d531c6fb3417374808f087eba94f48ade
Favour James <favourjames061@gmail.com>
2024-02-27 16:52:22 -0800

    5-save_to_json_file.py

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""
+A function that writes an ogject to a text file using
+JSON representation
+
+"""
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+
+    """Cresting a new file with write perm"""
+    with open(filename 'w') as json_file:
+
+        """returing the object file to text file"""
+        return json.dumps(my_obj, json_file)
