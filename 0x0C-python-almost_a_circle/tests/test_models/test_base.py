f0d855d693145c8e69ba28f297649e8a80be87eb
Favour James <favourjames061@gmail.com>
2024-03-03 14:51:55 -0800

    Task Rectangle

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -2,6 +2,7 @@
 
 import unittest
 from models.base import Base
+from models.rectangle import Rectangle
 
 
 """Creating a unittest for the Base module"""
@@ -38,6 +39,22 @@ class TestBase(unittest.TestCase):
 
         b = Base(-4)
         self.assertEqual(-4, b.id)
+
+"""
+
+***Priority
+I am now testing for Recangle module
+
+"""
+class TestRectangle(unittest.TestCase):
+
+    def test_width(self):
+
+        rect = Rectangle(2, 3, 4, 5, -6)
+        self.assertEqual(rect.id, -6)
+
+
+
 
 if __name__ == '__main__':
     unittest.main()
