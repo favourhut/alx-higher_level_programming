a85bc04b80d5636257f0dc17671f3559d1210b4f
Favour James <favourjames061@gmail.com>
2024-03-28 04:55:29 -0700

    script that updates the score of Bob

---
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,5 @@
+-- a script that updates the score of Bob to 10 in the table 
+-- not allowed to use Bob’s id value, only the name field
+UPDATE TABLE second_table
+SET score = 10
+WHERE name = Bob;
