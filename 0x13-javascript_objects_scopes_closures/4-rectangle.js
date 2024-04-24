0fea6564a1e101c8d33af3b40cb5223aa5cef921
Favour <135041430+favourhut@users.noreply.github.com>
2024-04-24 03:34:12 +0100

    Operator error solved

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -9,7 +9,7 @@ class Rectangle {
 
     // an instance method that prints the rectangle via character X
     print() {
-        for (let i = 0; i = (this.width); i++) {
+        for (let i = 0; i < (this.width); i++) {
             console.log('x'.repeat(this.height));
         }
     }
