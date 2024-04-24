d200e49d9df82d06dd44fad05149ce7341375388
Favour <135041430+favourhut@users.noreply.github.com>
2024-04-24 03:05:21 +0100

    Prints via X

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+class Rectangle {
+    constructor(w, h) {
+        if (w >= 0 && h >= 0) {
+            this.width = w;
+            this.height = h;
+        }
+    }
+
+    // an instance method that prints the rectangle via character X
+    print() {
+        for (let i = 0; i = (this.width); i++) {
+            let symbol = '';
+            for (let j = 0; j = (this.height); j++) {
+                symbol += 'X'
+            }
+            console.log(symbol)
+        }
+    }
+}
+
+module.exports = Rectangle;
+
