64d35202837e5823bccbb0dec6d6c38b22da0e6b
Favour <135041430+favourhut@users.noreply.github.com>
2024-05-22 20:33:14 +0100

    Don’t list rows without a name value

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,3 +1,6 @@
 -- Don’t list rows without a name value
 -- Records should be listed by descending score 
-SELECT name, score FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
+SELECT score, name 
+FROM second_table 
+WHERE name IS NOT NULL 
+ORDER BY score DESC;
