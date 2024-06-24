be14af751f3073a6076773a2f653361a738cb6f1
Favour <135041430+favourhut@users.noreply.github.com>
2024-06-24 14:27:40 +0100

    list all states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
             password=sys.argv[2], database=sys.argv[3], port="3306")
     
     db_cursor = db.cursor()
-    db_cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    db_cursor.execute("SELECT * FROM states")
     
     all_rows = db_cursor.fetchall()
     
