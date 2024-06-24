b4cb7d385aa8bfa7024fd1c65a1ddbe59095715a
Favour <135041430+favourhut@users.noreply.github.com>
2024-06-24 14:57:13 +0100

    Get all states from db

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,27 +1,34 @@
+# Write a script that lists all states from the database hbtn_0e_0_usa:
+
+#     Your script should take 3 arguments: mysql username, mysql password and database name (no argument validation needed)
+#     You must use the module MySQLdb (import MySQLdb)
+#     Your script should connect to a MySQL server running on localhost at port 3306
+#     Results must be sorted in ascending order by states.id
+#     Results must be displayed as they are in the example below
+#     Your code should not be executed when imported
+
 #!/usr/bin/python3
-"""script that lists all states from the database hbtn_0e_0_usa"""
+"""This script would list all 
+states from the database named"""
 
 import MySQLdb
-import sys
-
+from sys import argv
 
 if __name__ == "__main__":
-    
+    """This script takes 3 arguments: 
+    mysql username, mysql password and database name.
     """
-    This script list all states from the database
-    hbtn_0e_0_usa
-    """
-    db = MySQLdb.connect(
-            host="localhost", username=sys.argv[1],
-            password=sys.argv[2], database=sys.argv[3], port="3306")
+    data_base = MySQLdb.connect(
+        host="localhost", port="3306", username=argv[1], password=argv[2], 
+        database=argv[3])
     
-db_cursor = db.cursor()
-db_cursor.execute("SELECT * FROM states")
+    dbase_cursor = data_base.cursor()
+    dbase_cursor.execute("SELECT * FROM states ORDER BY id ASC")
     
-all_rows = db_cursor.fetchall()
+    fetch_rows = dbase_cursor.fetchall()
     
-for states_row in all_rows:
-    print(states_row)
-    
-db_cursor.close()
-all_rows.close()
+    for all_rows in fetch_rows:
+        print(all_rows)
+        
+    dbase_cursor.close()
+    data_base.close()
