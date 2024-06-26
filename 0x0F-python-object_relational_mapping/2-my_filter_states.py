201f331e82def1d3c0cf6add4f8336cf299973a8
Favour <135041430+favourhut@users.noreply.github.com>
2024-06-26 14:46:30 +0100

    Filter states by user input

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -9,7 +9,7 @@ from the database `hbtn_0e_0_usa`.
 import MySQLdb
 from sys import argv
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     """
     Access to the database and get the states
     from the database.
@@ -18,11 +18,8 @@ if __name__ == '__main__':
     conn = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
                          passwd=argv[2], db=argv[3])
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE name = {} ORDER BY states.id \
-                ASC".format(argv[4]))
+    cur.execute("SELECT * FROM states WHERE name = '{}'".format(argv[4]))
+    
     states_row = cur.fetchall()
     for rows in states_row:
         print(rows)
-        
-    cur.close()
-    conn.close()
