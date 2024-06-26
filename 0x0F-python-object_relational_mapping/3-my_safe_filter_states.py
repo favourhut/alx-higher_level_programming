a96ee90f99232ed82d190e7dfd5258b88fbb906c
Favour James <Favourjames061@gmail.com>
2024-06-26 06:59:47 -0700

    3-my_safe_filter_states.py

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+This script takes in an argument and
+displays all values in the states
+where `name` matches the argument
+from the database `hbtn_0e_0_usa`.
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    """
+    Access to the database and get the states
+    from the database.
+    """
+
+    conn = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
+                           passwd=argv[2], db=argv[3])
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states WHERE name
+            LIKE BINARY %s ORDER BY id ASC", (argv[4],))
+    
+    states_row = cur.fetchall()
+    for rows in states_row:
+        print(rows)
+
+    cur.close()
+    conn.close()
+
