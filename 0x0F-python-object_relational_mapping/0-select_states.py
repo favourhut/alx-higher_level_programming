d0edeb75e66551214bc6e6add6a3105049cddb58
Favour <135041430+favourhut@users.noreply.github.com>
2024-06-26 04:06:21 +0100

    Get all states

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""This script gets all states"""
+
+import MySQLdb
+from sys import argv
+
+
+if __name__ == "__main__":
+    """This script would get all states from the database
+    listed on the command line 
+    including passwords and username
+    """
+    db = MySQLdb.connect(
+        host="localhost", port="3306", username=argv[1], password=argv[2],
+        databse=agv[3])
+    
+    """adding cursor and rules"""
+    db_cursor = db.cursor()
+    db_cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    db_fetch = db_cursor.fetchall()
+    
+    """Iterating though db_fetch"""
+    for all_rows in db_fetch:
+        print(all_rows)
+        
+    db_cursor.close()
+    db.close()
