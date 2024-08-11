8e6c84e3d00333c7890a18fcf8d3111c72812cc7
Favour <135041430+favourhut@users.noreply.github.com>
2024-08-11 02:26:22 +0100

    Task 12 and 13 Completed

--- 0x15-javascript-web_jquery/103-script.js
+++ 0x15-javascript-web_jquery/103-script.js
@@ -1,8 +1,8 @@
 $('document').ready(function () {
     $('INPUT#btn_translate').click(translate);
     $('INPUT#language_code').focus(function () {
-      $(this).keydown(function (e) {
-        if (e.keyCode === 13) {
+      $(this).keydown(function (info) {
+        if (info.keyCode === 13) {
           translate();
         }
       });
