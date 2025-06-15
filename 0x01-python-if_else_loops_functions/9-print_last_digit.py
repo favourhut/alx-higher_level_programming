73a330f52b5cc072bac8a070eceb4b65df7c2c8f
Favour <135041430+favourhut@users.noreply.github.com>
2025-06-15 17:09:28 +0100

    Using the abs function to retrieve last digits

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    if number < 0:
-        result = (-number) % 10
-    elif result >= 0:
-        result = number % 10
-        return result
+    result = abs(number) % 10
+    return result
