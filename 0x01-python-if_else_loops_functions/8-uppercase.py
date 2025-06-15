052cc75f292002aa575d179170a3809b7030c51e
Favour <135041430+favourhut@users.noreply.github.com>
2025-06-15 16:41:04 +0100

    Prints letters in uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for letters in str:
+        if 97 <= ord(letters) <= 122:
+            storedLowerCase = letters - 32
+            print({}.format(storedLowerCase))
+        print({letters})
+    
