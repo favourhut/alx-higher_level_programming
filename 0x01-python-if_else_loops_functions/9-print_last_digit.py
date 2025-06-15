1789000e6129f5056a45c55869aa8cf276510c20
Favour <135041430+favourhut@users.noreply.github.com>
2025-06-15 17:02:07 +0100

    print last digits

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    if number < 0:
-        result = (-number) % 10
-    elif result >= 0:
-        result = number % 10
-        print('{:d}'.format(result), end="")
-        return result
+    for a in number:
+        if a == 0:
+            return a
+        else:
+            result = a % 10
+            return result
